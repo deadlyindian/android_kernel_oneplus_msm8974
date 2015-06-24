44696470ea07f6f33a3507b15d6cba115b57e8dd
anarkia1976 <stefano.villa1976@gmail.com>
2015-06-24 17:11:45 +0200

    drivers: state_notifier: Implement suspend_defer_timer
    * Thanks a lot neobuddy89
    * Suspend Defer Time prevents suspend routines wait X seconds to prevent
      false/very short suspend calls.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/video/backlight/lm3630_bl.c

--- drivers/video/backlight/lm3630_bl.c
+++ drivers/video/backlight/lm3630_bl.c
@@ -428,11 +428,11 @@ static void lm3630_backlight_unregister(struct lm3630_chip_data *pchip)
 #ifdef CONFIG_STATE_NOTIFIER
 	// if display is switched off
 	if (!use_fb_notifier && bl_level == 0)
-		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+		state_suspend();
 
 	// if display is switched on
 	if (!use_fb_notifier && bl_level != 0 && pre_brightness == 0)
-		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+		state_resume();
 #endif /*CONFIG_STATE_NOTIFIER*/
 
 /*Mobile Phone Software Dept.Driver, 2014/04/28  Add for add log for 14001 black screen */
